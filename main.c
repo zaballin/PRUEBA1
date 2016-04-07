8d197ce9e80eba86e538ce9ab6a2eafc4784930c
Edgar Ortiz <edgar.ortizl@ibero.mx>
2016-04-07 15:09:42 -0500

    Programa principal opcion, entradas

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int val1, val2;
+	float res;
+	int opc;
+
+//	opc=pide(int *val1, int *val2);
+	printf(" 1-suma, 2-resta, 3-mult, 4-div\n");
+	scanf("%d",&opc);
+	getchar();
+	printf("valor 1\n");
+	scanf("%d",&val1);
+	printf("valor 2\n");
+	scanf("%d",&val2);
+	
+
+        return 0;
+	
+}
