dc156992147dbbd46db45e5501cde04db8c9210d
Jose Antonio <jasza.94@gmail.com>
2016-04-22 12:04:44 -0500

    Función menu en otro archivo.
    Todas las funciones estan como extern, el menu va en un nuevo archivo, todas las opciones especifican que es y pide los valore spor separado si es el caso @eortizl

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h>
+/** 
 extern float suma(int a, int b);
 extern float resta(int a, int b);
-/** 
 extern float mult(int a, int b); 
 extern float expo(float a);
 extern float calculaPi(void);
@@ -11,9 +11,10 @@ extern long int potencia(int a, int b);
 extern float coseno(float a);
 extern float division(int a, int b);
 extern float serietaylor(float a, int b);
+int menu(void);
  **/
 
-int menu(void);
+
 int main(void)
 {
         int val1, val2;
@@ -27,60 +28,75 @@ int main(void)
           switch (opc)
           {
         case 1:
-                printf("dame dos valores\n");
+        	printf("\tSUMA\n\n")
+                printf("dame un valor:\n");
                 scanf("%d",&val1);
+                 printf("dame un otro:\n");
                 scanf("%d",&val2);
                 res = suma(val1,val2);
                 printf("La suma es %f\n",res);
                 break;
 	case 2:
-                printf("dame dos valores\n");
+                printf("\tRESTA\n\n")
+                printf("dame un valor:\n");
                 scanf("%d",&val1);
+                 printf("dame un otro:\n");
                 scanf("%d",&val2);
                 res = resta(val1,val2);
                 printf("La resta es %f\n",res);
                 break;
         case 3:
-                printf("dame dos valores\n");
+                printf("\tMULTIPLICACION\n\n")
+                printf("dame un valor:\n");
                 scanf("%d",&val1);
+                 printf("dame un otro:\n");
                 scanf("%d",&val2);
                 res = mult(val1,val2);
                 printf("La multiplicacion es %f\n",res);
                 break;
 	case 4:
+		printf("\tPI\n\n")
                 res = calculaPi();
                 printf("PI es igual a: %f\n",res);
                 break;
 	case 5:
-                printf("dame un valor\n");
+		printf("\tEXPONENCIAL\n\n")
+                printf("dame un valor:\n");
                 scanf("%f",&f1);
                 res = expo(f1);
                 printf("EL resultado es: %f\n",res);
                 break;
 	case 6:
-                printf("dame dos valores\n");
+               printf("\tPOTENCIA\n\n")
+                printf("dame un valor:\n");
                 scanf("%d",&val1);
+                 printf("dame un otro:\n");
                 scanf("%d",&val2);
                 res_potencia = potencia(val1,val2);
                 printf("La resultado es %lu\n",res_potencia);
                 break;
 	case 7:
-                printf("dame un valor\n");
+		printf("\tCOSENO\n\n")
+                printf("dame un valor:\n");
                 scanf("%f",&f1);
                 res = coseno(f1);
                 printf("EL resultado es: %f\n",res);
                 break;
 case 8:
-                printf("dame dos valores\n");
+                printf("\tDIVISION\n\n")
+                printf("dame un valor:\n");
                 scanf("%d",&val1);
+                printf("dame un otro:\n");
                 scanf("%d",&val2);
                 res = division(val1,val2);
                 printf("La division es %f\n",res);
                 break;
 case 9:
-                printf("dame dos valores\n");
+                printf("\tEL SENO DE TAYLOR\n\n")
+                printf("dame un valor:\n");
+                scanf("%d",&val1);
+                 printf("dame un otro:\n");
                 scanf("%f",&f1);
-                scanf("%d",&val2);
                 res = sen_tay(f1,val2);
                 printf("El resultado es %f\n",res);
                 break;
@@ -93,22 +109,4 @@ case 9:
   
         return 0;
 
-}
-int menu(void)
-{
-        int opc=0;
-    do
-     {
-        printf("Menu\n");
-        printf("Opcion \n");
-        printf("1-Suma \n2-Resta \n3-Multiplica\n4-Pi\n5-Exponencial\n6-Potenca\n7-Coseno\n8-Division\n9-Serie de Taylor\n");
-        printf("10-Salir\n");
-        if(scanf("%d",&opc)!=1)
-        {
-          printf("La opcion debe ser numerica, reintenta\n");
-        }
-        printf("----- %d \n",opc);
-        while(getchar()!='\n');
-    }while(opc == 0);   
-    return opc;
 }
